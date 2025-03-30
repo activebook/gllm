d1c5c49a00a9f3ece96bef495539fc1db7e18e2d
Charles Liu <xinasuka@126.com>
2025-03-30 21:03:34 +0800

    .

--- gllm.rb
+++ gllm.rb
@@ -1,9 +1,9 @@
 class Gllm < Formula
     desc "`gllm` is a powerful command-line tool designed to interact seamlessly with various Large Language Models (LLMs). Configure your API keys, set your preferred models, and start chatting or executing commands effortlessly."
     homepage "https://github.com/activebook/gllm"
-    url "https://github.com/activebook/gllm/archive/v1.0.0.tar.gz"
-    sha256 "the-sha256-checksum-of-your-release-tarball"
-    license "MIT" # or whatever license you're using
+    url "https://github.com/activebook/gllm/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "360edc47362b1dd0c82a4fd09f880f942832674cc7c45abf7978e2ad203d948c"
+    license "Apache-2.0" # or whatever license you're using
   
     depends_on "go" => :build
   
