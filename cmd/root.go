045e5dd65459d8fdd58b0c99c8464c96fbe1a3dd
Charles Liu <xinasuka@126.com>
2025-04-08 10:14:43 +0800

    build: update version number to v1.6.0
    - Move version string from root.go to version.go
    - Update version number from v1.5.1 to v1.6.0
    - Refactor version flag handling for better maintainability

--- cmd/root.go
+++ cmd/root.go
@@ -15,9 +15,7 @@ import (
 )
 
 var (
-	// Hardcode the version string here
-	version     = "v1.5.1" // <<< Set your desired version
-	versionFlag bool       // To hold the version flag value
+	versionFlag bool // To hold the version flag value
 
 	cfgFile           string // To hold the path to the config file if specified via flag
 	appConfigDir      string // Store the calculated config directory path
