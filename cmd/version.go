567f8dfaa327cd97a841af83ce01ce40e60fcf4f
Charles Liu <xinasuka@126.com>
2025-04-08 10:38:08 +0800

    Hotfix on chat session filename

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Hardcode the version string here
-const version = "v1.6.0" // <<< Set your desired version
+const version = "v1.6.1" // <<< Set your desired version
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
